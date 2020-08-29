eb4264c15b2746e9ffb3ee16d43b2f676525af0a
Piyush Jasaiwal <coderpiyush123@gmail.com>
2020-08-30 02:45:18 +0530

    made some change

--- problems/Permutations.java
+++ problems/Permutations.java
@@ -10,15 +10,18 @@ public static void main(String [] args){
         System.out.println("1");;
     }else if(n == 2 || n == 3){
         System.out.println("NO SOLUTION");
-    }else{
+    }else if(n == 4){
+        System.out.println("2 4 1 3");
+    }
+    
+    else{
         for(int i = 1;i<=n;i+=2){
-            ans += ""+ i +" ";
+            System.out.print(i+" ");
         }
 
         for(int i = 2;i<=n;i+=2){
-            ans += ""+ i +" ";
+            System.out.print(i+" ");
         }
-        System.out.println(ans);
     }
 }
 }
